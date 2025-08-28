021f0d938fac9f6f3ab642538b2611a9fe275220
gzmyldrm <gzmyldrm@yahoo.com>
2025-08-28 07:07:43 +0000

    prediction

--- src/code_location_interview/code_location_interview/assets/magenta_interview/predict.py
+++ src/code_location_interview/code_location_interview/assets/magenta_interview/predict.py
@@ -1,6 +1,7 @@
 import logging
 import sys
 
+from dagster import AssetIn, asset
 from dagster import get_dagster_logger
 
 
@@ -9,13 +10,22 @@ log_datefmt = "%Y-%m-%d %H:%M:%S"
 logging.basicConfig(stream=sys.stdout, format=log_fmt, datefmt=log_datefmt, level=logging.INFO)
 logger = get_dagster_logger(__name__)
 
-# group_name = "predict"
+group_name = "predict"
 
 
-# @asset(
-#     group_name=group_name,
-#     
-# )
-# def predictions(classifier, df_input_preprocessed):
-# 
-#     return predictions_df
+@asset(
+    group_name=group_name,
+    ins={
+        "rf_model": AssetIn(),
+        "X_test": AssetIn(),
+        "feature_selection": AssetIn()
+    }
+)
+def predictions(rf_model, X_test, feature_selection):
+
+    predictions_df = rf_model.predict_proba(X_test[feature_selection])
+
+    logger.info(f"Generated predictions for {len(predictions_df)} instances.")
+    logger.info(f"Predictions sample: {predictions_df[:5]}")
+
+    return predictions_df
